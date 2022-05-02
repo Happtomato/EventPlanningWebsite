788d65c6d93c19c50382996efc728b9d376b199b
Happtomato <40277552+Happtomato@users.noreply.github.com>
2022-05-02 11:03:17 +0200

    fix of validate user function

--- dyoevents/ServerSide/ValidateUser.php
+++ dyoevents/ServerSide/ValidateUser.php
@@ -1,9 +1,13 @@
 <?php
 
+require_once ("currentUser.php");
 class ValidateUser{
+
+    public $user = null;
+
+
     function userIsValid($userLogin,$userPassword){
 
-        echo "test";
         //create connection to db
         require_once("DBController.php");
         $db_handle = new DBController();
@@ -14,33 +18,59 @@ class ValidateUser{
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
         }
-        echo "test";
+
+        //now working
 
         $sql = "SELECT UserPassword FROM `UserAccounts` WHERE UserLogin = '$userLogin'";
         $result = $conn->query($sql);
         $conn->close();
 
-        echo "test";
+
         $hashArray = $result->fetch_all();
-        echo "test";
         $hash = $hashArray[0][0];
-        echo "test";
-        echo $hash;
         if (password_verify($userPassword,$hash)) {
 
+
             if($this->getUserType($userLogin) == "admin"){
-                $this->user = new currentUser($userLogin,$userPassword);
-                include "AdminPage.php";
+                $user = new currentUser($userLogin,$userPassword);
+
+                echo "admin da";
+                header("Location: AdminPage.php");
 
             }
             else{
-
-                include "MemberPage.php";
+                echo "user da";
+                header("Location: MemberPage.php");
             }
         } else {
             echo "Benutzer wurde nicht gefunden";
+            header("Location: ../ClientSide/LogIn.html");
         }
 
+    }
+    function getUserType($userLogin)
+    {
+        //create connection to db
+        require_once("DBController.php");
+        $db_handle = new DBController();
+
+        $conn = $db_handle->connectDB();
+
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+        $sql = "SELECT userType FROM UserAccounts WHERE UserLogin = '$userLogin'";
+        $result = $conn->query($sql);
+
+        $conn->close();
+
+        $resultArray = $result->fetch_all();
+        return $resultArray[0][0];
+
+
+        echo "error";
+        return "";
     }
 
     function getUserID($userLogin)
@@ -69,27 +99,5 @@ class ValidateUser{
         echo "error";
         return "";
     }
-    function getUserType($userLogin)
-    {
-            //create connection to db
-            require_once("dbcontroller.php");
-            $db_handle = new DBController();
 
-            $conn = $db_handle->connectDB();
-
-            // Check connection
-            if ($conn->connect_error) {
-                die("Connection failed: " . $conn->connect_error);
-            }
-
-            $sql = "SELECT userType FROM UserAccounts WHERE userLogin = '$userLogin'";
-            $result = $conn->query($sql);
-
-            $conn->close();
-
-            return $result;
-
-        echo "error";
-        return "";
-    }
 }
