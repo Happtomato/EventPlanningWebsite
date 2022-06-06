b25fca49589ef8109784b505468bc9ad68f88861
Ahmad-El-Hajj-Chehade <A.ElHajjChehade.inf20@stud.bbbaden.ch>
2022-06-06 11:48:40 +0200

    Update MemberPage.php

--- dyoevents/MemberPage.php
+++ dyoevents/MemberPage.php
@@ -3,6 +3,19 @@ session_start();
 if(isset($_SESSION['user_type'])) {
 $user = strtok($_SESSION['login'], '@');
 $userid = $_SESSION['login'];
+$id = $_SESSION["id"];/* userid of the user */
+
+if(count($_POST)>0) {
+$result = mysqli_query($con,"SELECT *from student WHERE name='" . $id . "'");
+$row=mysqli_fetch_array($result);
+if($_POST["currentPassword"] == $row["password"] && $_POST["newPassword"] == $row["confirmPassword"] ) {
+mysqli_query(connectDB(),"UPDATE student set password='" . $_POST["newPassword"] . "' WHERE name='" . $id . "'");
+$message = "Password Changed Sucessfully";
+} else{
+ $message = "Password is not correct";
+}
+}
+
 ?>
 
 
@@ -58,9 +71,9 @@ $userid = $_SESSION['login'];
     </header>
     <h1><?php echo "Hello ".$user ; ?></h1>
     <div id = "product-grid">
-        <div id = "txt-heading">Deine Produkte</div>
+        <!--<div id = "txt-heading">Deine Produkte</div>!-->
     <br>
-    <?php
+    <!--<?php
                 $orders_array = $db_handle->runQuery("SELECT OrderDate FROM Orders");
                 if (!empty($orders_array)) {
                     foreach ($orders_array as $key => $value) {
@@ -81,6 +94,24 @@ $userid = $_SESSION['login'];
                     echo $value["Product_ID"];
                 }
                 ?>
+!-->
+<h3 >CHANGE PASSWORD</h3>
+<div><?php if(isset($message)) { echo $message; } ?></div>
+<form method="post" action="" >
+Current Password:<br>
+<input type="password" name="currentPassword"><span id="currentPassword" class="required"></span>
+<br>
+New Password:<br>
+<input type="password" name="newPassword"><span id="newPassword" class="required"></span>
+<br>
+Confirm Password:<br>
+<input type="password" name="confirmPassword"><span id="confirmPassword" class="required"></span>
+<br><br>
+<input type="submit">
+</form>
+<br>
+
+<br>
 </body>
 
 </html>
